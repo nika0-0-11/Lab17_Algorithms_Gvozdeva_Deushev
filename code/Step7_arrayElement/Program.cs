3d9e530b1f35be3d19b5586b29bd618687e03625
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-03-22 17:28:42 +0300

    Step 7 arrays

---
+++ code/Step7_arrayElement/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("=== Вывод элементов массива ===");
+
+int[] array = {1, 51, 34, 812, 21};
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
---
+++ code/Step7_arrayMax/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("=== Поиск максимального элемента ===");
+
+int[] array = {1, 51, 34, 812, 21};
+
+int max = array[0];
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    } 
+}
+Console.WriteLine($"Максимальный элемент: {max}");
