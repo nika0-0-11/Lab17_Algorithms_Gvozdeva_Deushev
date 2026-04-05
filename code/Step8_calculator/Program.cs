e58c40487255834491cb563cd47153161dbd2fe8
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-04-05 16:10:53 +0300

    Step 8: functions and modular programming

---
+++ code/Step8_calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿double Sum(double a, double b)
+{
+    return a + b;
+}
+
+double Sub(double a, double b)
+{
+    return a - b;
+}
+
+double Mul(double a, double b)
+{
+    return a * b;
+}
+
+double Div(double a, double b)
+{
+    return a / b;
+}
+
+Console.Write("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double result1 = Sum(a, b); 
+double result2 = Sub(a, b);
+double result3 = Mul(a, b);
+double result4 = Div(a, b);
+
+Console.WriteLine($"Sum: {result1}");
+Console.WriteLine($"Sub: {result2}");
+Console.WriteLine($"Mul: {result3}");
+Console.WriteLine($"Div: {result4}");
+
+
+// Console.WriteLine($"Сложение:  {Sum(a, b)}");
+// Console.WriteLine($"Вычитание: {Sub(a, b)}");
+// Console.WriteLine($"Умножение: {Mul(a, b)}");
+// Console.WriteLine($"Деление: {Div(a, b)}");
